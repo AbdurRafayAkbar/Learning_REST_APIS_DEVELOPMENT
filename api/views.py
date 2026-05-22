3c2c9fd535a3313a2e9be0968bf0bd642e0d1b92
Abdur Rafay Akbar <abdurrafaym10@gmail.com>
2026-05-22 16:12:29 +0500

    Created Serializer and url paths

--- api/views.py
+++ api/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
-# Create your views here.
+from .serializers import StudentSerializer
+from students.models import StudentModel
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
+@api_view["GET"]
+def student_list(request):
+    students=StudentModel.objects.all()
+    serializer=StudentSerializer(students,many=True)
+    return Response(serializer.data,status=status.HTTP_200_OK)
