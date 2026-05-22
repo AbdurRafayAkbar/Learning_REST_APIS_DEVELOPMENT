50d5cdcff1b4c9144cfe57e680d1df2e8b5aa434
Abdur Rafay Akbar <abdurrafaym10@gmail.com>
2026-05-22 16:29:01 +0500

    Updated API project files

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from students.models import StudentModel
 
-# Register your models here.
+admin.site.register(StudentModel)
--- api/views.py
+++ api/views.py
@@ -4,8 +4,9 @@ from students.models import StudentModel
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import status
-@api_view["GET"]
+@api_view(["GET"])
 def student_list(request):
-    students=StudentModel.objects.all()
-    serializer=StudentSerializer(students,many=True)
-    return Response(serializer.data,status=status.HTTP_200_OK)
+    if request.method=="GET":
+        students=StudentModel.objects.all()
+        serializer=StudentSerializer(students,many=True)
+        return Response(serializer.data,status=status.HTTP_200_OK)
