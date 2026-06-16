27986a9bdaa52e41d495c5ff46110e2d93054f36
Abdur Rafay Akbar <abdurrafaym10@gmail.com>
2026-06-16 12:36:52 +0500

    Created Classse and Functionalities

--- Single_Student_APi/student/views.py
+++ Single_Student_APi/student/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from .models import StudentModel
+from .serializers import StudentSerializer
 from rest_framework import mixins,generics
 
+class Students(mixins.ListModelMixin,mixins.CreateModelMixin,generics.GenericAPIView):
+    queryset=StudentModel.objects.all()
+    serializer_class = StudentSerializer 
+
+    def get(self,request):
+        return self.list(request)
+    def post(self,request):
+        return self.create(request)
+
+class SingleStudent(mixins.DestroyModelMixin,mixins.RetrieveModelMixin,mixins.UpdateModelMixin,generics.GenericAPIView):
+    queryset=StudentModel.objects.all()
+    serializer_class = StudentSerializer
+
+    def get(self,request):
+        return self.retrieve(request)
+    
+    def put(self,request):
+        return self.update(request)
+    
+    def delete(self,request):
+        return self.destroy(request)
