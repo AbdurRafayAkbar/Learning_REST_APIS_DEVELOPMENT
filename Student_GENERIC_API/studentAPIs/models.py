f15d55a8ace14c9aa381599232f9c786d640d31b
Abdur Rafay Akbar <abdurrafaym10@gmail.com>
2026-06-16 15:58:38 +0500

    Created Serializer and modelsin the project

--- Student_GENERIC_API/studentAPIs/models.py
+++ Student_GENERIC_API/studentAPIs/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+ 
+class Students(models.Model):
+    name=models.CharField(max_length=50)
+    email=models.EmailField(unique=True)
 
-# Create your models here.
+    def __str__(self):
+        return self.name
