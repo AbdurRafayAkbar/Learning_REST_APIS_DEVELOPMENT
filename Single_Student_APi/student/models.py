e272c034e43cd25c8f5fd3706611fd61f6ca2d56
Abdur Rafay Akbar <abdurrafaym10@gmail.com>
2026-06-16 10:48:38 +0500

    Created Database for student APi

--- Single_Student_APi/student/models.py
+++ Single_Student_APi/student/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Degree(models.TextChoices):
+    Software="Software Engineering"
+    Computer="Computer Science"
+    BBA="Business Administration"
+
+class StudentModel(models.Model):
+    name=models.CharField(max_length=50)
+    email=models.EmailField(unique=True)
+    age=models.IntegerField()
+    Degree_selected=models.CharField(max_length=50,choices=Degree,default=Degree.Software)
+    created_at=models.DateTimeField()
