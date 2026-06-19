c9303b942394bd70dbae4bd64b699560bdc4795e
Abdur Rafay Akbar <abdurrafaym10@gmail.com>
2026-06-19 22:27:56 +0500

    Added Max Age to bo on 60 years

--- Employ_data_VIEWSETS/employes_api/models.py
+++ Employ_data_VIEWSETS/employes_api/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.core.validators import MaxValueValidator,MinValueValidator
 class Employees(models.Model):
 
     department_choices=[
@@ -9,7 +9,7 @@ class Employees(models.Model):
     ]
     name=models.CharField(max_length=100)
     email=models.EmailField(unique=True)
-    age=models.IntegerField()
+    age=models.IntegerField(validators=[MaxValueValidator(60)])
     department=models.CharField(max_length=100,default="Software Engineering",choices=department_choices)
 
     def __str__(self):
