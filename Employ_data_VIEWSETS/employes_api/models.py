96fbf39a0dcc72060e4288b6f34094f62e446a32
Abdur Rafay Akbar <abdurrafaym10@gmail.com>
2026-06-19 22:31:03 +0500

    Added salary in database

--- Employ_data_VIEWSETS/employes_api/models.py
+++ Employ_data_VIEWSETS/employes_api/models.py
@@ -11,6 +11,7 @@ class Employees(models.Model):
     email=models.EmailField(unique=True)
     age=models.IntegerField(validators=[MaxValueValidator(60),MinValueValidator(18)])
     department=models.CharField(max_length=100,default="Software Engineering",choices=department_choices)
+    salary=models.IntegerField()
 
     def __str__(self):
         return self.name
