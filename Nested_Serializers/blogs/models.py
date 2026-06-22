31208a89b11ef20028dc6a9fa4751984d303c76b
Abdur Rafay Akbar <abdurrafaym10@gmail.com>
2026-06-22 16:53:25 +0500

    created Models in App, Used Foreign Key

--- Nested_Serializers/blogs/models.py
+++ Nested_Serializers/blogs/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Blogs(models.Model):
+    blog_title=models.CharField(max_length=100)
+    blog_text=models.TextField()
+
+    def __str__(self):
+        return self.blog_title
+
+class Comments(models.Model):
+    blogs = models.ForeignKey(Blogs,on_delete=models.CASCADE,related_name="comments")
+    comments=models.TextField()
+
+    def __str__(self):
+        return self.blogs
