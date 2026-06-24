2c48ef18c6dbab7642984b7f0dfda1aee7b742ee
Abdur Rafay Akbar <abdurrafaym10@gmail.com>
2026-06-24 12:57:08 +0500

    changed small changes

--- Nested_Serializers/blogs/serializers.py
+++ Nested_Serializers/blogs/serializers.py
@@ -1,8 +1,8 @@
 from .models import Blogs,Comments
 from rest_framework import serializers
 
+
 class comments_serializer(serializers.ModelSerializer):
-    
     class Meta:
         model=Comments
         fields="__all__"
@@ -12,4 +12,3 @@ class blog_serializer(serializers.ModelSerializer):
     class Meta:
         model=Blogs
         fields="__all__"
-
