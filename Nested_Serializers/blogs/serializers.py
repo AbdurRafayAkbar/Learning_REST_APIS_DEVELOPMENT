13254ea8d6bd205df5745bd8a4239b1b5115a0b8
Abdur Rafay Akbar <abdurrafaym10@gmail.com>
2026-06-24 15:42:27 +0500

    Change the serializer formation. blog_title and blog_text

--- Nested_Serializers/blogs/serializers.py
+++ Nested_Serializers/blogs/serializers.py
@@ -5,10 +5,10 @@ from rest_framework import serializers
 class comments_serializer(serializers.ModelSerializer):
     class Meta:
         model=Comments
-        fields="__all__"
+        fields=['comments']
 
 class blog_serializer(serializers.ModelSerializer):
     comments=comments_serializer(many=True,read_only=True)
     class Meta:
         model=Blogs
-        fields="__all__"
+        fields=['id','blog_title','blog_text','comments']
