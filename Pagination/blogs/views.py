a4ced82bef3bcb85cee854e9898c555dd01db367
Abdur Rafay Akbar <abdurrafaym10@gmail.com>
2026-06-25 11:55:35 +0500

    Added custom pagination in views.py

--- Pagination/blogs/views.py
+++ Pagination/blogs/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render
 from rest_framework import generics
 from .models import Blogs
 from.serializers import Blog_Serializer
+from .pagination import Custom_Paginnation
 
 class Blogs_View(generics.ListCreateAPIView):
     queryset=Blogs.objects.all()
     serializer_class=Blog_Serializer
+    pagination_class=Custom_Paginnation
 
 class Detailed_Blog_View(generics.RetrieveUpdateDestroyAPIView):
     queryset=Blogs.objects.all()
